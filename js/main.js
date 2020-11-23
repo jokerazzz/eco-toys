73d9cb6874b8fa38df3e53ee34a444f73f8b69d7
jokerazzz <youiever.ao@gmail.com>
2020-11-24 02:49:29 +0600

    adaptive

--- js/main.js
+++ js/main.js
@@ -31,6 +31,9 @@ $('.btn-catalog').on('click', function(){
   $('.mobile-catalog').toggleClass('active');
 });
 
+$('.aside__list').on('click', function(){
+  $('.aside_top').toggleClass('active');
+});
 
 $(function(){
   $('input[type="number"]').niceNumber();
@@ -39,21 +42,103 @@ $(function(){
 
 $('.slider__container').slick({
   prevArrow:'<button class="slick-prev" type="button"><img src="images/main/arrow-left.png" alt=""></button>',
-  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>'
+  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>',
+  
+  responsive: [
+    
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // centerMode: true,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 $('.new__container').slick({
   slidesToShow:4,
   slidesToScroll:1,
   prevArrow:'<button class="slick-prev" type="button"><img src="images/main/arrow-white-left.png" alt=""></button>',
-  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-white-right.png" alt=""></button>'
+  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-white-right.png" alt=""></button>',
+  responsive: [
+    
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    },
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
+  ]
 });
+
+
 
 $('.hit__container').slick({
   slidesToShow:4,
   slidesToScroll:1,
   prevArrow:'<button class="slick-prev" type="button"><img src="images/main/arrow-left.png" alt=""></button>',
-  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>'
+  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>',
+  responsive: [
+    
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: true
+      }
+    }
+  ]
 });
 
 $('.reviwes__container').slick({
@@ -65,7 +150,19 @@ $('.reviwes__container').slick({
 $('.clients__container').slick({
   slidesToShow:2,
   prevArrow:'<button class="slick-prev" type="button"><img src="images/main/arrow-left.png" alt=""></button>',
-  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>'
+  nextArrow:'<button class="slick-next" type="button"><img src="images/main/arrow-right.png" alt=""></button>',
+  responsive: [
+    
+    
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    }
+  ]
 });
 
 
